621cc2f5f87c0d71d11716458ca04f7d7e7a3f7d
刘炳文 <liubingwen@huianrong.com>
2017-12-08 10:01:15 +0800

    扩展单一项目自定义baseURL

--- example/api/demo/index.js
+++ example/api/demo/index.js
@@ -1,65 +1,65 @@
 import instance from './instance'
 import { convertRESTAPI } from '../util'
 
-/** 支持 restful 的 mock，替换 id 试试 */
-function restfulIdListGet(opts) {
-  return instance({
-    method: 'get',
-    url: convertRESTAPI('/restful/:id/list', opts),
-    opts: opts
-  })
-}
-
-/** 支持接口代理的 mock，试试在 url 上加 ?s={数字} */
-function proxyGet(opts) {
-  return instance({
-    method: 'get',
-    url:  '/proxy',
-    opts: opts
-  })
-}
-
-/** 带随机数据的 mock */
-function mockGet(opts) {
-  return instance({
-    method: 'get',
-    url:  '/mock',
-    opts: opts
-  })
-}
-
-/** 普通的 mock */
-function userGet(opts) {
-  return instance({
-    method: 'get',
-    url:  '/user',
-    opts: opts
-  })
-}
-
-/** 这只是一个响应 post 接口返回随机数据的例子 */
-function uploadPost(opts) {
+/** 微信签名 */
+function wxGetWxSignParPost(opts) {
   return instance({
     method: 'post',
-    url:  '/upload',
+    url:  '/wx/getWxSignPar',
     opts: opts
   })
 }
 
-/** 根据请求参数返回指定数据，试试在 url 上加 ?name={任意值} */
-function queryGet(opts) {
+/** 领取奖品 */
+function double12ReceivePrizePost(opts) {
+  return instance({
+    method: 'post',
+    url:  '/double12/receivePrize',
+    opts: opts
+  })
+}
+
+/** 提交投保订单 */
+function double12SubmitInsOrderPost(opts) {
+  return instance({
+    method: 'post',
+    url:  '/double12/submitInsOrder',
+    opts: opts
+  })
+}
+
+/** template */
+function double12QueryCodePost(opts) {
+  return instance({
+    method: 'post',
+    url:  '/double12/queryCode',
+    opts: opts
+  })
+}
+
+/** 手机品牌列表 */
+function double12BrandListGet(opts) {
   return instance({
     method: 'get',
-    url:  '/query',
+    url:  '/double12/brandList',
+    opts: opts
+  })
+}
+
+/** 地区数据 */
+function double12AreaListGet(opts) {
+  return instance({
+    method: 'get',
+    url:  '/double12/areaList',
     opts: opts
   })
 }
 
 export {
-  restfulIdListGet,
-  proxyGet,
-  mockGet,
-  userGet,
-  uploadPost,
-  queryGet
+  wxGetWxSignParPost,
+  double12ReceivePrizePost,
+  double12SubmitInsOrderPost,
+  double12QueryCodePost,
+  double12BrandListGet,
+  double12AreaListGet
 }
---
+++ example/api/wx/index.js
@@ -0,0 +1,15 @@
+import instance from './instance'
+import { convertRESTAPI } from '../util'
+
+/** 微信签名 */
+function getWxSignParPost(opts) {
+  return instance({
+    method: 'post',
+    url:  '/getWxSignPar',
+    opts: opts
+  })
+}
+
+export {
+  getWxSignParPost
+}
